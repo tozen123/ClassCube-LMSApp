0b5245fed6afce91799fa7edf26d8b97d4d6c292
tozen123 <lanzuela.christian@gmail.com>
2023-04-06 16:21:54 +0800

    changes

--- app/src/main/java/com/doublehammerstudios/classcube/LoginActivity.java
+++ app/src/main/java/com/doublehammerstudios/classcube/LoginActivity.java
@@ -1,23 +1,101 @@
 package com.doublehammerstudios.classcube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView btn_forgotPassword;
     TextView btn_dontHaveAnAccount;
+
+    EditText inputmEmail, inputmPassword;
+    Button btn_Login;
+    FirebaseAuth mAuth;
+
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        inputmEmail = (EditText) findViewById(R.id.edTxt_email_login);
+        inputmPassword = (EditText) findViewById(R.id.edTxt_password_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        btn_Login = findViewById(R.id.btn_logintxt);
+        progressDialog = new ProgressDialog(this);
+
         btn_forgotPassword = (TextView) findViewById(R.id.btn_forgotPasswordtxt);
         btn_dontHaveAnAccount = (TextView) findViewById(R.id.btn_doesntHaveAnAccountTxt);
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputmEmail.getText().toString();
+                String password = inputmPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    inputmEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    inputmPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    inputmPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                Log.d("TAG", "EMAIL: "+email);
+                Log.d("TAG", "PASSWORD: "+password);
+
+                progressDialog.setMessage("Logging in...");
+                progressDialog.setTitle("Log-In");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+
+                    }
+                });
+
+            }
+        });
 
         btn_forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +112,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/doublehammerstudios/classcube/RegisterActivity.java
+++ app/src/main/java/com/doublehammerstudios/classcube/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,15 +20,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = "TAG";
@@ -37,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
     EditText inputName, inputEmail, inputPassword, inputConfirmPassword;
 
     RadioGroup rdTypeOfUser;
+    RadioButton selectedRadioButton;
     Button btnRegister;
 
     String emailPattern = "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
@@ -46,19 +50,17 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseUser mUser;
     FirebaseFirestore fStore;
     String userID;
+    String typeOfUser = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         mUser = mAuth.getCurrentUser();
 
-        //Send user to the main_activity if already login
-        if(mUser != null){
-            setActivity();
-            finish();
-        }
+        rdTypeOfUser = (RadioGroup) findViewById(R.id.rdg_typeUser);
 
         inputName = (EditText) findViewById(R.id.edtxt_name);
         inputEmail = (EditText) findViewById(R.id.edtxt_email);
@@ -71,6 +73,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister = (Button) findViewById(R.id.btn_createAccount);
 
         progressDialog = new ProgressDialog(this);
+
         btn_alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,32 +102,49 @@ public class RegisterActivity extends AppCompatActivity {
     private void performAuthentication() {
         String name = inputName.getText().toString();
         String email = inputEmail.getText().toString();
-        String password = inputName.getText().toString();
+        String password = inputPassword.getText().toString();
         String confirmPassword = inputConfirmPassword.getText().toString();
+
+
+        int selectedRadioButtonId = rdTypeOfUser.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            selectedRadioButton = findViewById(selectedRadioButtonId);
+            typeOfUser = selectedRadioButton.getText().toString();
+        }
+
         if(name.isEmpty()){
             inputName.setError("Goddamn it!, Enter your name!");
         } else if(!email.matches(emailPattern)){
             inputEmail.setError("Goddamn it!, Enter correct email!");
         } else if (password.isEmpty() || password.length()<6){
             inputPassword.setError("Goddamn it!, Enter proper password!");
-        } else if(password.equals(confirmPassword)){
+        } else if(!password.equals(confirmPassword)){
             inputConfirmPassword.setError("Goddamn it!, Password does not matched!");
+        } else if(typeOfUser.isEmpty()){
+            Toast.makeText(this, "Goddamn it!, Choose what type user!", Toast.LENGTH_SHORT).show();
         } else {
             progressDialog.setMessage("Registering...");
             progressDialog.setTitle("Create Account");
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
 
+            Log.d("SUPERTAGGER", "EMAIL: "+email);
+            Log.d("SUPERTAGGER", "PASSWORD: "+password);
+            Log.d("SUPERTAGGER", "TYPEOFUSER: "+typeOfUser);
+
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
+
                         progressDialog.dismiss();
-                        userID = mUser.getUid();;
+
+                        userID = mAuth.getCurrentUser().getUid();
                         DocumentReference documentReference = fStore.collection("users").document(userID);
                         Map<String,Object> user = new HashMap<>();
-                        user.put("fName",name);
-                        user.put("email",email);
+                        user.put("Name",name);
+                        user.put("Email",email);
+                        user.put("TypeOfUser", typeOfUser);
                         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
